b9c7a19488be417ad4ed05f8465d83a709de2496
Santiago Vela <Santiago Vela@DESKTOP-KIIP8R7>
2021-02-19 16:38:05 -0600

    Mi primer comint.

---
+++ Git - 66204/src/git/pkg66204/Git66204.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.pkg66204;
+
+/**
+ *
+ * @author Santiago Vela
+ */
+public class Git66204 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     String nombre = "SANTIAGO JOEL";
+     String matricula = "66204";
+        System.out.println("SOY" + nombre + "Y MI MATRICULA ES" + matricula);
+     
+    //Acabo de crear mi repositorio local en team
+    }
+    
+}
